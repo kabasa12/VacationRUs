384d9616b1b6a4ad7312e3266e619c3261b6eac8
kabasa12 <kabasa12@gmail.com>
2020-10-13 17:43:00 +0300

    new Add

--- node/utils/database.js
+++ node/utils/database.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql2');
-
-var con = mysql.createConnection(process.env.DATABASE_URL || {
+var CLEARDB_DATABASE_URL = `mysql2://adffdadf2341:adf4234@us-cdbr-east.cleardb.com/heroku_db?reconnect=true`
+var con = mysql.createConnection(CLEARDB_DATABASE_URL || {
     host: "localhost",
     user: "root",
     password: "",
